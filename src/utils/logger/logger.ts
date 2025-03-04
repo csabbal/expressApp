bc5f6740ec33e14748086724ded4c8dddaf4600e
Csabán Balázs <Ba.Csaban@eos-ts.com>
2025-03-04 14:03:26 +0100

    [logging] - added basic Logger class

---
+++ src/utils/logger/logger.ts
@@ -0,0 +1,50 @@
+import { createLogger, format, transports } from 'winston'
+import DailyLogRotate, { GeneralDailyRotateFileTransportOptions } from 'winston-daily-rotate-file'
+
+
+
+class LoggerClass {
+    level = 'info'
+    logger: any
+    constructor() {
+        this.init()
+    }
+
+    format = format.combine(
+        format.colorize(),
+        format.timestamp(),
+        format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`)
+    )
+
+    init() {
+        this.logger = createLogger({
+            level: this.level,
+            format: this.format,
+            transports: [
+                new transports.Console(),
+                new transports.File({ filename: './logs/info.log' }),
+                this.getLogRotateTransport('info'),
+            ]
+        })
+    }
+
+    getLogRotateTransport(level: string) {
+        return new DailyLogRotate({
+            dirname: './logs',
+            filename: `${level}%DATE%.log`,
+            format: this.format,
+            datePattern: 'YYYYMMDD',
+            zippedArchive: true,
+            maxFiles: '30d',
+            maxSize: '200m',
+            auditFile: `./logs/.audit.json`,
+            createSymlink: true,
+            symlinkName: `./logs/info.log`
+        })
+    }
+}
+
+
+export const logger = new LoggerClass().logger
+
+
