664f14367b2c0595591c345c5c2d0e0464c19590
Csabán Balázs <Ba.Csaban@eos-ts.com>
2025-03-04 15:03:21 +0100

    [logging] - set requestIdMiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import router from './routes/index'
+import AsyncLocalStorageClass from './utils/asyncLocalStorage/asyncLocalStorage';
 
 
 //For env File 
@@ -8,6 +9,8 @@ dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+
+app.use(AsyncLocalStorageClass.requestIdMiddleware)
 
 app.use('/api',router)
 
